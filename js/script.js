0dc7593d23890191f72ef79cef2538ee28f59c6a
Alina Gladkova <a-g1997@yandex.ru>
2022-01-16 18:11:11 +0500

    Header links  animation added

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// const paths = document.querySelectorAll('.text__line path');
+
+// paths.forEach((path) => {
+//   const length = path.getTotalLength();
+
+//   path.style.transition = path.style.WebkitTransition = 'none';
+
+//   path.style.strokeDasharray = length + ' ' + length;
+//   path.style.strokeDashoffset = length;
+
+//   path.getBoundingClientRect();
+
+//   path.style.transition = path.style.WebkitTransition =
+//     'stroke-dashoffset 5s ease-in-out';
+
+//   path.style.strokeDashoffset = '0';
+// });

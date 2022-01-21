8d09546400f7e009f292a5a45ce3972b16b41d69
Alina Gladkova <a-g1997@yandex.ru>
2022-01-21 18:16:54 +0500

    Gallery added

--- js/script.js
+++ js/script.js
@@ -2,26 +2,6 @@
 
 const html = document.querySelector('html');
 const body = document.querySelector('body');
-const openVideoBtns = document.querySelectorAll('.hero__play-btn');
-
-const playVideo = function () {
-  openVideoBtns.forEach((btn) =>
-    btn.addEventListener('click', function () {
-      const video = `<div class="video__wrapper">
-    <video src="/videos/video.mp4" class="video" preload="none" poster="/images/content/poster.jpg" controls autoplay></video>
-    <div class="overlay"><div>
-    </div>`;
-      body.insertAdjacentHTML('afterbegin', video);
-      html.classList.add('disable');
-      const overlay = document.querySelector('.overlay');
-      const wrapper = document.querySelector('.video__wrapper');
-      overlay.addEventListener('click', function () {
-        html.classList.remove('disable');
-        wrapper.remove();
-      });
-    })
-  );
-};
 
 const observeLogo = function () {
   window.addEventListener('scroll', function () {
@@ -30,7 +10,7 @@ const observeLogo = function () {
   });
 };
 
-const observeGallery = function () {
+const observeGallerySection = function () {
   const options = {
     rootMargin: '-100px',
   };
@@ -116,6 +96,143 @@ const lazyLoad = function () {
   }
 };
 
+const playVideo = function () {
+  const openVideoBtns = document.querySelectorAll('.hero__play-btn');
+  openVideoBtns.forEach((btn) =>
+    btn.addEventListener('click', function () {
+      const video = `<div class="video__wrapper">
+    <video src="/videos/video.mp4" class="video" preload="none" poster="/images/content/poster.jpg" controls autoplay></video>
+    <div class="overlay"><div>
+    </div>`;
+      body.insertAdjacentHTML('afterbegin', video);
+      html.classList.add('disable');
+      const overlay = document.querySelector('.overlay');
+      const wrapper = document.querySelector('.video__wrapper');
+      overlay.addEventListener('click', function () {
+        html.classList.remove('disable');
+        wrapper.remove();
+      });
+    })
+  );
+};
+
+const openGallery = function () {
+  const images = document.querySelectorAll('.gallery__img');
+  images.forEach((image) => {
+    image.addEventListener('click', function () {
+      const gallery = `<div class="gallery__wrapper wrapper">
+  <img
+    src="/images/content/gallery-img-1.jpg"
+    alt=""
+    class="wrapper__img"
+  /><img
+    src="/images/content/gallery-img-2.jpg"
+    alt=""
+    class="wrapper__img"
+  /><img
+    src="/images/content/gallery-img-3.jpg"
+    alt=""
+    class="wrapper__img"
+  /><img
+    src="/images/content/gallery-img-4.jpg"
+    alt=""
+    class="wrapper__img"
+  /><img
+    src="/images/content/gallery-img-5.jpg"
+    alt=""
+    class="wrapper__img"
+  /><img
+    src="/images/content/gallery-img-6.jpg"
+    alt=""
+    class="wrapper__img"
+  /><img
+    src="/images/content/gallery-img-7.jpg"
+    alt=""
+    class="wrapper__img"
+  /><img
+    src="/images/content/gallery-img-8.jpg"
+    alt=""
+    class="wrapper__img"
+  /><img
+    src="/images/content/gallery-img-9.jpg"
+    alt=""
+    class="wrapper__img"
+  /><img
+    src="/images/content/gallery-img-10.jpg"
+    alt=""
+    class="wrapper__img"
+  /><img
+    src="/images/content/gallery-img-11.jpg"
+    alt=""
+    class="wrapper__img"
+  /><img
+    src="/images/content/gallery-img-12.jpg"
+    alt=""
+    class="wrapper__img"
+  /><img
+    src="/images/content/gallery-img-13.jpg"
+    alt=""
+    class="wrapper__img"
+  /><img
+    src="/images/content/gallery-img-14.jpg"
+    alt=""
+    class="wrapper__img"
+  /><img
+    src="/images/content/gallery-img-15.jpg"
+    alt=""
+    class="wrapper__img"
+  /><img
+    src="/images/content/gallery-img-16.jpg"
+    alt=""
+    class="wrapper__img"
+  /><img src="/images/content/gallery-img-17.jpg" alt="" class="wrapper__img" />
+  <div class="overlay"></div>
+</div>`;
+      body.insertAdjacentHTML('afterbegin', gallery);
+      html.classList.add('disable');
+      const overlay = document.querySelector('.overlay');
+      const wrapper = document.querySelector('.gallery__wrapper');
+      const generateDarkColorRgb = function () {
+        const red = Math.floor(Math.random() * 256);
+        const green = 0;
+        const blue = 0;
+        return 'rgb(' + red + ', ' + green + ', ' + blue + ')';
+      };
+      const observeBackgound = function () {
+        const options = {
+          root: null,
+          rootMargin: '0px',
+          threshold: 0,
+        };
+
+        const observeCallback = function (entries, observer) {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              wrapper.style.background = generateDarkColorRgb();
+            }
+          });
+        };
+        const observer = new IntersectionObserver(observeCallback, options);
+
+        const targets = document.querySelectorAll('.wrapper__img');
+        targets.forEach((target) => {
+          observer.observe(target);
+        });
+      };
+      observeBackgound();
+
+      overlay.addEventListener('click', function () {
+        html.classList.remove('disable');
+        wrapper.remove();
+      });
+      wrapper.addEventListener('wheel', (evt) => {
+        evt.preventDefault();
+        wrapper.scrollLeft += evt.deltaY;
+      });
+    });
+  });
+};
+
 const showContent = function () {
   const header = document.querySelector('.header');
   const hero = document.querySelector('.hero');
@@ -134,11 +251,12 @@ const showContent = function () {
 const init = function () {
   showContent();
   observeLogo();
-  observeGallery();
+  observeGallerySection();
   observeImages();
   observeText();
   lazyLoad();
   playVideo();
+  openGallery();
 };
 
 document.querySelectorAll('a').forEach((anchor) => {
